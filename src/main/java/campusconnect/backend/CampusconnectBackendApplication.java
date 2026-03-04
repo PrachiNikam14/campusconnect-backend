9afb2bfdded7871043e28729edfdad6c9ad185bd
PRACHI NIKAM <prachinikam2005@gmail.com>
2026-03-04 20:13:44 +0530

    Initial backend setup with Spring Boot and Supabase PostgreSQL

---
+++ src/main/java/campusconnect/backend/CampusconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package campusconnect.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusconnectBackendApplication.class, args);
+	}
+
+}
